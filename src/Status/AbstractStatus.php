c64793c6bdf500369b8aec088e479fbe3d6cd05a
Thomas Abplanalp <info@tasoft.ch>
2020-07-05 13:28:52 +0200

    ADD: predefines status

---
+++ src/Status/AbstractStatus.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Copyright (c) 2020 TASoft Applications, Th. Abplanalp <info@tasoft.ch>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+namespace Ikarus\SPS\Visualizer\Brick\Status;
+
+
+abstract class AbstractStatus implements StatusInterface
+{
+	/** @var string|int */
+	protected $id;
+	/** @var int */
+	protected $status;
+
+	/**
+	 * AbstractStatuc constructor.
+	 * @param int|string $id
+	 * @param int $status
+	 */
+	public function __construct($id, int $status)
+	{
+		$this->id = $id;
+		$this->status = $status;
+	}
+
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getID()
+	{
+		return $this->id;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getStatus(): int
+	{
+		return $this->status;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function toJSON(): string
+	{
+		return json_encode( $this->getJSONFields(), JSON_UNESCAPED_SLASHES );
+	}
+
+	/**
+	 * Gets a list with the JSON properties to be encoded.
+	 *
+	 * @return array
+	 */
+	protected function getJSONFields(): array {
+		return [
+			'id' => $this->getID(),
+			'status' => $this->getStatus()
+		];
+	}
+}
---
+++ src/Status/AttributedBrickStatus.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ * Copyright (c) 2020 TASoft Applications, Th. Abplanalp <info@tasoft.ch>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+namespace Ikarus\SPS\Visualizer\Brick\Status;
+
+
+class AttributedBrickStatus extends BrickStatus
+{
+	/** @var array  */
+	protected $attributes = [];
+
+	/**
+	 * AttributedBrickStatus constructor.
+	 * @param string $id
+	 * @param int $status
+	 * @param array $attributes
+	 */
+	public function __construct(string $id, int $status, array $attributes = [])
+	{
+		parent::__construct($id, $status);
+		$this->attributes = $attributes;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function serialize()
+	{
+		return serialize([
+			$this->id,
+			$this->status,
+			$this->attributes
+		]);
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function unserialize($serialized)
+	{
+		list($this->id, $this->status, $this->attributes) = unserialize($serialized);
+	}
+
+	/**
+	 * @return array
+	 */
+	public function getAttributes(): array
+	{
+		return $this->attributes;
+	}
+
+	/**
+	 * Defines a new attribute
+	 *
+	 * @param $name
+	 * @param $value
+	 * @return static
+	 */
+	public function setAttribute($name, $value) {
+		$this->attributes[$name] = $value;
+		return $this;
+	}
+
+	/**
+	 * Removes an attribute
+	 *
+	 * @param $name
+	 * @return static
+	 */
+	public function removeAttribute($name) {
+		unset($this->attributes[$name]);
+		return $this;
+	}
+}
